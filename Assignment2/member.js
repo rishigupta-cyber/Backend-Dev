60e90b80d508509ad1185d5b54d3a51505ab74f8
Rishi Gupta <rishiguptaorai04@gmail.com>
2026-02-11 11:49:46 +0530

    Remove unused folders from file-system-operations branch

--- Assignment2/member.js
+++
@@ -1,9 +0,0 @@
-function createMember(memberId, name, membershipType) {
-    return {
-        memberId: memberId,
-        name: name,
-        membershipType: membershipType
-    }
-}
-
-export default { createMember }
